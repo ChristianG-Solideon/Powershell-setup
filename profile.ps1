d88a2703922c200971b61639a1a6858635f9b69e
Christian Gouldy <christian.gouldy@solideon.com>
2025-10-27 12:58:05 -0700

    init for powershell setup

---
+++ profile.ps1
@@ -0,0 +1,12 @@
+$script:debug_Override = $false
+
+function Get-Theme_Override {
+    $ompConfigPath = Join-Path ([Environment]::GetFolderPath("MyDocuments")) "Powershell/my_layout.omp.json"
+    if (-not (Test-Path $ompConfigPath)) {
+        Write-Warning "Oh My Posh configuration file not found at $ompConfigPath."
+        return
+    }
+
+    $ompInit = oh-my-posh init pwsh --config $ompConfigPath
+    Invoke-Expression $ompInit
+}
